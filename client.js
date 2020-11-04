4bbc67b0dd042851f31850cb6e94068266a31c3e
Erica <ericasu33@gmail.com>
2020-11-04 20:02:45 +0000

    log message on connection on client end and writes initial on snake on server end

--- client.js
+++ client.js
@@ -12,6 +12,11 @@ const connect =  function() {
     console.log('Server says: ', data);
   });
 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: ESU");
+  });
+
   return conn;
 };
 
