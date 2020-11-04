7854e74106baa1d9d1bf2cec70e25799c01c1692
Erica <ericasu33@gmail.com>
2020-11-04 21:29:39 +0000

    set up the basis for UI where player can send keyboard commands, and made sure we can exit with ctrl + c

--- play.js
+++ play.js
@@ -1,5 +1,25 @@
+const { stdin } = require('process');
 const { connect } = require('./client');
+
+const handleUserInput = function() {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+};
+
 
 
 console.log("Connecting....");
 connect();
+setupInput();
