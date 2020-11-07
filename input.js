8a9efe2d164623246f5c7db5b2bd18cdc3d416c9
Erica <ericasu33@gmail.com>
2020-11-07 23:30:35 +0000

    modified how conn is called

--- input.js
+++ input.js
@@ -1,18 +1,16 @@
 const { stdin } = require('process');
 const { moves } = require('./constants');
-let conn;
 
-const setupInput = function(connection) {
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  conn = connection;
-  handleUserInput();
+  handleUserInput(conn);
   return stdin;
 };
 
-const handleUserInput = function() {
+const handleUserInput = function(conn) {
   stdin.on('data', (key) => {
     if (moves[key] !== undefined) {
       conn.write(moves[key]);
